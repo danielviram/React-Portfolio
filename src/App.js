e54b8b62cf9022918aab6b225cd7b6f0740b52db
Daniel Viramontes <danielviramo@gmail.com>
2020-12-01 14:14:53 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "./Colors.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import NotFoundComponent from "./components/NotFoundComponent";
+import HomePageContainer from "./containers/HomePageContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePageContainer} />
+          <Route path="/404" component={NotFoundComponent} />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
